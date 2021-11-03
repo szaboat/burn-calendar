f13d778a5d5aef7f0f2908f926d948808ed06fd8
Attila Szabo <attila.szabo@gmail.com>
2021-11-03 09:23:13 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const MONTHS = 18;
+  const [range, setRange] = useState([...Array(18 * 4).keys()]);
+  const [unit, setUnit] = useState("week");
+  const label = unit.trim().replace(/^\w/, (c) => c.toUpperCase());
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Burn Calendar for {MONTHS} months</h1>
+        <h2>
+          by&nbsp;
+          <select
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value === "month") {
+                setRange([...Array(MONTHS).keys()]);
+                setUnit("month");
+              } else if (value === "week") {
+                setRange([...Array(MONTHS * 4).keys()]);
+                setUnit("week");
+              } else if (value === "day") {
+                setRange([...Array(MONTHS * 4 * 7).keys()]);
+                setUnit("day");
+              }
+            }}
+          >
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+            <option value="day">Day</option>
+          </select>
+        </h2>
       </header>
+
+      <section>
+        {range.map((item) => {
+          return (
+            <div className={unit}>
+              {label}
+              <br />
+              {range.length - item}
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
